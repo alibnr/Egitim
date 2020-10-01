8d92cfb9cf8121bac1936610b99ff452997f6cbb
alibiner <ali_bnr@outlook.com>
2020-10-01 17:14:06 +0300

    onSubmit

--- src/FormDemo.js
+++ src/FormDemo.js
@@ -8,16 +8,21 @@ export default class FormDemo extends Component {
         this.setState({userName: event.target.value})
 
     }
+    onSubmit = (event) => {
+        event.preventDefault();
+        alert(this.state.userName)
+    }
 
     
 
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit= {this.onSubmit}>
                     <h3>User Name</h3>
                     <input type="text" onChange={this.changeName}></input>
                     <h3>User Name : {this.state.userName} </h3>
+                    <input type = "submit" value="Save"></input>
                 </form>
             </div>
         )
