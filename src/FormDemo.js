adb3f42b0b5fe4841d152d0b95748bc407ae51c3
alibiner <ali_bnr@outlook.com>
2020-10-01 17:22:03 +0300

    name

--- src/FormDemo.js
+++ src/FormDemo.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 
 export default class FormDemo extends Component {
     state={
-        userName: ""
+        userName: "",
+        city:""
     }
     changeName= (event) => {
-        this.setState({userName: event.target.value})
+        //this.setState({userName: event.target.value})
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({[name]:value})
 
     }
     onSubmit = (event) => {
@@ -20,8 +24,13 @@ export default class FormDemo extends Component {
             <div>
                 <form onSubmit= {this.onSubmit}>
                     <h3>User Name</h3>
-                    <input type="text" onChange={this.changeName}></input>
+                    <input name = "userName" type="text" onChange={this.changeName}></input>
                     <h3>User Name : {this.state.userName} </h3>
+
+                    <h3>City</h3>
+                    <input name="city" type="text" onChange={this.changeName}></input>
+                    <h3>City : {this.state.city} </h3>
+
                     <input type = "submit" value="Save"></input>
                 </form>
             </div>
